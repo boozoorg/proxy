96afbe28774189586b695c75329df155f1147b32
boozoorg <boozoorg22@gmail.com>
2024-08-30 18:06:27 +0500

    hot fix x2

--- proxy.go
+++ proxy.go
@@ -11,8 +11,9 @@ func HTTP(w http.ResponseWriter, r *http.Request, c *http.Client) error {
 	return handleHTTP(w, r, c)
 }
 
-// WS note: if request URL contain domain name (not ip address) than
-// add suffix :http/:https (for func net.Dial)
+// WS note: if request URL contain domain name (not ip address)
+// without http(or https) scheme than add suffix :http(or :https)
+// (for func net.Dial)
 func WS(w http.ResponseWriter, r *http.Request) error {
 	return handleWS(w, r)
 }
